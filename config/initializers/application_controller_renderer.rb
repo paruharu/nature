5e9c6de67b5a7d6575ef0be026bba3189347bc9f
paruharu <oha.ru.sunny0861@gmail.com>
2022-01-30 10:09:21 +0000

    [update]画像

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -4,3 +4,4 @@
 #   http_host: 'example.org',
 #   https: false
 # )
+Refile.secret_key = '909e2781cbb95270c6759e44684543d3be47d688795165855d92a32b33798a9460b5b2d390c9c9817f405fab20a2b8d9dc611d7dd2f49764f3c944e522a1bfde'
